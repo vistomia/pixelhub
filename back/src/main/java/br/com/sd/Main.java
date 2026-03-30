695d1a5b710f5fdd695121cae486875f2be14a65
emanueldias01 <emanueldias01dev@gmail.com>
2026-03-30 17:11:45 -0300

    remove main

--- back/src/main/java/br/com/sd/Main.java
+++ back/src/main/java/br/com/sd/Main.java
@@ -1,50 +1,9 @@
 package br.com.sd;
 
-import br.com.sd.channel.PixelInputStream;
-import br.com.sd.domain.board.Board;
-import br.com.sd.domain.pixel.Pixel;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Board board = new Board(1000, 1000);
-
-        try {
-            ServerSocket server = new ServerSocket(8080);
-
-            while (true) {
-                Socket client = server.accept();
-
-                handleClient(client, board);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void handleClient(Socket client, Board board) {
-        try {
-            PixelInputStream inputStream =
-                    new PixelInputStream(client.getInputStream());
-
-            while (true) {
-
-                Pixel p = inputStream.readPixel();
-
-                board.setPixel(p.getX(), p.getY(), p.getColor());
-
-                System.out.println("Pixel atualizado: (" +
-                        p.getX() + "," + p.getY() + ") -> " + p.getColor());
-            }
-
-        } catch (IOException e) {
-            System.out.println("Cliente desconectado.");
-        }
     }
 }
