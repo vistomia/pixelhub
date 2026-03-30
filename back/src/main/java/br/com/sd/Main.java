1e93257dcb5f4c4212cd02a0459d1a9469fa0ecf
emanueldias01 <emanueldias01dev@gmail.com>
2026-03-30 17:37:44 -0300

    init code server

--- back/src/main/java/br/com/sd/Main.java
+++ back/src/main/java/br/com/sd/Main.java
@@ -1,9 +1,23 @@
 package br.com.sd;
 
 
+import br.com.sd.infra.Server;
+
+import java.io.IOException;
+import java.net.Socket;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(8080);
 
+        while(true){
+            Socket connection = server.getServerSocket().accept();
+
+            new Thread(() -> {
+                server.resolveConnection(connection);
+            }).start();
+        }
     }
 }
--- back/src/main/java/br/com/sd/infra/Server.java
+++ back/src/main/java/br/com/sd/infra/Server.java
@@ -1,4 +1,34 @@
 package br.com.sd.infra;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    private List<Socket> connections = new ArrayList<>();
+
+    public Server(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public void resolveConnection(Socket connection) {
+        connections.add(connection);
+
+        //mantem conexao e espera alteracoes, em caso de alteracao, manda pra todo mundo com sendModifysForClients
+    }
+
+    public void sendModifysForClients() {
+        this.connections.forEach(c -> {
+            //manda alteracoes para todo mundo
+        });
+    }
 }
